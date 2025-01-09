fd00ce08cf4393f17b690208d31f5d9d17cef9da
Dmytro Rozhko <d.rozhko.ua@gmail.com>
2025-01-09 15:07:00 +0200

    added solution auth app

--- library/authentication/views.py
+++ library/authentication/views.py
@@ -1,1 +1,66 @@
 # Create your views here.
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponse
+from .models import CustomUser
+from django.views.decorators.csrf import csrf_exempt
+
+
+# Registration view
+def register(request):
+    if request.method == "POST":
+        first_name = request.POST.get("first_name")
+        middle_name = request.POST.get("last_name")
+        last_name = request.POST.get("last_name")
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+
+        if not first_name or not last_name or not email or not password:
+            return HttpResponse("All fields are required!", status=400)
+
+        if CustomUser.get_by_email(email):
+            return HttpResponse("User with this email already exists!", status=400)
+
+        user = CustomUser.create(
+            email=email,
+            password=password,
+            first_name=first_name,
+            middle_name=middle_name,
+            last_name=last_name,
+        )
+
+        if user:
+            return redirect("login")
+        return HttpResponse("Failed to create user!", status=500)
+
+    return render(request, "authentication/register.html")
+
+
+# Login view
+def user_login(request):
+    if request.method == "POST":
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+
+        user = CustomUser.get_by_email(email=email)
+        if user and user.password == password:
+            user.backend = "django.contrib.auth.backends.ModelBackend"
+            login(request, user)  # Логін
+            return redirect("home")
+        return HttpResponse("Invalid credentials!", status=401)
+
+    return render(request, "authentication/login.html")
+
+
+# Logout view
+def user_logout(request):
+    if request.user.is_authenticated:
+        logout(request)
+        return redirect("login")
+    return HttpResponse("User not logged in!", status=400)
+
+
+def home(request):
+    if not request.user.is_authenticated:
+        return redirect("login")
+    return render(request, "authentication/home.html")
--- library/library/settings.py
+++ library/library/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '_y2b#-m(nwf8irkpgs)wpg+-e$#_7^xaevp^me4+u4ov+3fyw*'
+SECRET_KEY = "_y2b#-m(nwf8irkpgs)wpg+-e$#_7^xaevp^me4+u4ov+3fyw*"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -31,49 +31,49 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'authentication',
-    'author',
-    'book',
-    'order',
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "authentication",
+    "author",
+    "book",
+    "order",
 ]
 
-AUTH_USER_MODEL = 'authentication.CustomUser'
+AUTH_USER_MODEL = "authentication.CustomUser"
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = 'library.urls'
+ROOT_URLCONF = "library.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'library.wsgi.application'
+WSGI_APPLICATION = "library.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
@@ -87,13 +87,13 @@ WSGI_APPLICATION = 'library.wsgi.application'
 
 DATABASE = "simpledbmamager"
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'localhost',
-        'PORT': '5432',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "postgres",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
@@ -102,25 +102,25 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "UTC"
 
 USE_I18N = True
 
@@ -131,34 +131,51 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = "/static/"
 
 # Logger required settings.
 
 LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'console': {
-            'class': 'logging.StreamHandler',
-            'formatter': 'default',
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+            "formatter": "default",
         },
     },
-    'loggers': {
-        'dev': {
-            'handlers': ['console'],
-            'level': 'DEBUG',
-            'propagate': True,
+    "loggers": {
+        "dev": {
+            "handlers": ["console"],
+            "level": "DEBUG",
+            "propagate": True,
         },
     },
-    'formatters': {
-        'default': {
-            'format': '%(levelname)s %(asctime)s %(pathname)s line: %(lineno)d '
-                      'message: %(message)s',
-            'datefmt': '%d/%m/%Y %I:%M:%S'
+    "formatters": {
+        "default": {
+            "format": "%(levelname)s %(asctime)s %(pathname)s line: %(lineno)d "
+            "message: %(message)s",
+            "datefmt": "%d/%m/%Y %I:%M:%S",
         },
     },
 }
+
+LOGIN_URL = "/auth/login/"
+LOGIN_REDIRECT_URL = "/auth/home/"
+LOGOUT_REDIRECT_URL = "/auth/login/"
+
+SESSION_ENGINE = "django.contrib.sessions.backends.db"
+SESSION_COOKIE_AGE = 1209600
+SESSION_SAVE_EVERY_REQUEST = True
+
+SESSION_COOKIE_SECURE = False
+SESSION_COOKIE_DOMAIN = None
+
+
+AUTHENTICATION_BACKENDS = [
+    "django.contrib.auth.backends.ModelBackend",
+]
+
 try:
     from .local_settings import *
 except ImportError:
